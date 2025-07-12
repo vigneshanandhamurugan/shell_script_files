4f73020ab2782b45f4b977acbf6313cce237febc
vicky <avicky21121999@gmail.com>
2025-07-12 13:03:13 +0530

    fs

---
+++ shell_script/Sample.java
@@ -0,0 +1,8 @@
+public class Sample{
+    public static void main(int[] ar) {
+        System.out.println(ar[0]);
+    }
+    public static void main(String[] ar) {
+        System.out.println(ar[0]+ar[1]);
+    }
+}
